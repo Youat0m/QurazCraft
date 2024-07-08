4e94f8c1e3f81dba3ffe40c5c11e8daad180dacc
youAt0m <senkaved@gmail.com>
2024-07-08 23:57:13 +0300

    add blackstone

--- src/main/java/com/youat0m/quarz/QuarzCraft.java
+++ src/main/java/com/youat0m/quarz/QuarzCraft.java
@@ -4,6 +4,7 @@ import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.BlastingRecipe;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapelessRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class QuarzCraft extends JavaPlugin {
@@ -13,7 +14,12 @@ public final class QuarzCraft extends JavaPlugin {
         NamespacedKey key = new NamespacedKey(this, "QuarzCraft");
 
         BlastingRecipe recipe = new BlastingRecipe(key, new ItemStack(Material.QUARTZ), Material.DIORITE, 1, 60);
+        ShapelessRecipe recipe1 = new ShapelessRecipe(new NamespacedKey(this, "ChernitCraft"),
+                new ItemStack(Material.BLACKSTONE, 3));
+        recipe1.addIngredient(2, Material.COBBLESTONE);
+        recipe1.addIngredient(2, Material.COAL);
         getServer().addRecipe(recipe);
+        getServer().addRecipe(recipe1);
 
     }
 
